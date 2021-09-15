0f996e6a97d0c928c90e9d977afbbf1fd6b9ae0d
imansaadat <e.saadat72@gmail.com>
2021-09-16 03:39:35 +0430

    index and styles updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 /* for menu responsive */ 
 
-let hamMenu = document.querySelector(".hamburger");
+let hamMenu = document.querySelector(".icon-menu");
 let menu = document.querySelector(".list_items");
+let body = document.querySelector('body');
 hamMenu.addEventListener("click", () => {
   menu.classList.toggle("show");
+  body.classList.toggle('overflow')
+  if(hamMenu.className !=='icon-cancel'){
+    hamMenu.className ='icon-cancel'
+  }
+  else{
+    hamMenu.className ='icon-menu';
+  }
 });
