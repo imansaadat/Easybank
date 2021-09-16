7ad38d20d968fad13a09bce0727e0b8ae66f465c
imansaadat <e.saadat72@gmail.com>
2021-09-17 01:41:25 +0430

    styles and js updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,8 @@
 
 let hamMenu = document.querySelector(".icon-menu");
 let menu = document.querySelector(".list_items");
-let body = document.querySelector('body');
 hamMenu.addEventListener("click", () => {
   menu.classList.toggle("show");
-  body.classList.toggle('overflow')
   if(hamMenu.className !=='icon-cancel'){
     hamMenu.className ='icon-cancel'
   }
